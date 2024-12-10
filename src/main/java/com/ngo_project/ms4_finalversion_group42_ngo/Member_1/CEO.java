3f42a4f486a3251fca8317252153803a5cda5fac
SsRito <102437346+SsRito@users.noreply.github.com>
2024-12-11 04:18:47 +0600

    init commit

--- src/main/java/com/ngo_project/ms4_finalversion_group42_ngo/Member_1/CEO.java
+++ src/main/java/com/ngo_project/ms4_finalversion_group42_ngo/Member_1/CEO.java
@@ -1,6 +1,9 @@
 package com.ngo_project.ms4_finalversion_group42_ngo.Member_1;
 
+import java.time.LocalDate;
+
 public class CEO {
     private String name;
     private int user_id;
+    private LocalDate dob;
 }
