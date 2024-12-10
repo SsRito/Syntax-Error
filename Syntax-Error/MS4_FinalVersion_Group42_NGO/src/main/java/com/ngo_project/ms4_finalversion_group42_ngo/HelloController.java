6e0b2010b62fcd63388ce5906706732535fb7fcc
SsRito <102437346+SsRito@users.noreply.github.com>
2024-12-11 02:44:08 +0600

    Add files via upload

---
+++ Syntax-Error/MS4_FinalVersion_Group42_NGO/src/main/java/com/ngo_project/ms4_finalversion_group42_ngo/HelloController.java
@@ -0,0 +1,14 @@
+package com.ngo_project.ms4_finalversion_group42_ngo;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ Syntax-Error/MS4_FinalVersion_Group42_NGO/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.ngo_project.ms4_finalversion_group42_ngo {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.ngo_project.ms4_finalversion_group42_ngo to javafx.fxml;
+    exports com.ngo_project.ms4_finalversion_group42_ngo;
+}
